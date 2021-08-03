6829f4c1a6e4c3ac6421c5ac72724746d73dcdc1
Suzie K <suziekim.dev@gmail.com>
2021-08-02 21:06:50 -0400

    Green test for forecast details

--- app/poros/forecast_details.rb
+++ app/poros/forecast_details.rb
@@ -6,8 +6,50 @@ class ForecastDetails
 
   def initialize(details)
     @id = details[:id] 
-    @current_weather = details[:current]
-    @daily_weather = details[:daily].first
-    @hourly_weather = details[:hourly].first
+    @current_weather = get_current_weather(details[:current])
+    @daily_weather = get_daily_weather(details[:daily][0..4])
+    @hourly_weather = get_hourly_weather(details[:hourly][0..7])
   end
+
+  private
+
+    def get_current_weather(data)
+      {
+        datetime: Time.at(data[:dt]).to_s(:dt),
+        sunrise: Time.at(data[:sunrise]).to_s(:time),
+        sunset: Time.at(data[:sunset]).to_s(:time),
+        temperature: data[:temp],
+        feels_like: data[:feels_like],
+        humidity: data[:humidity],
+        uvi: data[:uvi],
+        visibility: data[:visibility],
+        conditions: data[:weather][0][:description],
+        icon: data[:weather][0][:icon]
+      }
+    end
+
+    def get_daily_weather(data)
+      data.map do |data|
+        {
+          date: Time.at(data[:dt]).to_date,
+          sunrise: Time.at(data[:sunrise]).to_s(:time),
+          sunset: Time.at(data[:sunset]).to_s(:time),
+          max_temp: data[:temp][:max],
+          min_temp: data[:temp][:min],
+          conditions: data[:weather][0][:description],
+          icon: data[:weather][0][:icon]
+        }
+      end
+    end
+
+    def get_hourly_weather(data)
+      data.map do |data|
+        {
+          time: Time.at(data[:dt]).to_s(:time),
+          temperature: data[:temp],
+          conditions: data[:weather][0][:description],
+          icon: data[:weather][0][:icon]
+        }
+      end
+    end
 end
