8c3a98ee29b67da7c1370745b4bab0d98cf76c5c
Suzie K <suziekim.dev@gmail.com>
2021-08-09 13:20:03 -0400

    Refactor Facade methods

--- app/facades/city_breweries_facade.rb
+++ app/facades/city_breweries_facade.rb
@@ -1,19 +1,32 @@
 class CityBreweriesFacade 
+
   def self.get_city_breweries(location, quantity)
     coordinates = MapQuestFacade.coordinates(location)
     breweries = BreweriesService.find_breweries(coordinates, quantity)
     data = OpenWeatherService.get_forecast_data(coordinates)
     forecast = ForecastDetails.new(data)
-
-    farenheit = ((forecast.current_weather[:temperature] - 273.15) * 9/5 + 32).round
+    farenheit = new.convert_to_f(forecast.current_weather[:temperature])
+    city_breweries = new.format_breweries(breweries)
+    details = new.format_details(location, forecast, farenheit, city_breweries)
+    CityBreweriesDetails.new(details)
+  end
   
-    city_breweries = breweries.map do |brewery|
-      {id: brewery[:id],
-       name: brewery[:name],
-       brewery_type: brewery[:brewery_type]}
-    end
+  def convert_to_f(kelvin)
+    ((kelvin - 273.15) * 9/5 +32).round
+  end
 
-    details = {
+  def format_breweries(breweries)
+    breweries.map do |brewery|
+      {
+       id: brewery[:id],
+       name: brewery[:name],
+       brewery_type: brewery[:brewery_type]
+      }
+    end
+  end
+
+  def format_details(location, forecast, farenheit, city_breweries)
+    {
       destination: location, 
       forecast: {
         summary: forecast.current_weather[:conditions],
@@ -21,7 +34,5 @@ class CityBreweriesFacade
       },
       breweries: city_breweries
     }
-
-    CityBreweriesDetails.new(details)
   end
 end
