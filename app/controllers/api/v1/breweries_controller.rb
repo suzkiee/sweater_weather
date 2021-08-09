c2fb445a8a5721b515d9b063b3bb8c527188ddc3
Suzie K <suziekim.dev@gmail.com>
2021-08-09 13:06:17 -0400

    Green test add sad path

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::BreweriesController < ApplicationController
   def index 
-    city_breweries = CityBreweriesFacade.get_city_breweries(params[:location], params[:quantity])  
-    render json: CityBreweriesSerializer.new(city_breweries), status: 200
+    city_breweries = CityBreweriesFacade.get_city_breweries(params[:location], params[:quantity]) 
+    
+    if params[:quantity].to_i <= 0
+      render json: { error: "Quantity cannot be less than 0" }, status: 400
+    else 
+      render json: CityBreweriesSerializer.new(city_breweries), status: 200
+    end
   end
 end
