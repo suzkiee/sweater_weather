5de96cd191123de2e67cffe8d3a6dea6fa713336
Suzie K <suziekim.dev@gmail.com>
2021-08-09 18:50:33 -0400

    Remove validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,5 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:api_key) }
-    it { should validate_uniqueness_of(:api_key) }
   end
 end
