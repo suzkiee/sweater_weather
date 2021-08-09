f485e347260ffdb8c4df4fe2211848ceb640c509
Suzie K <suziekim.dev@gmail.com>
2021-08-09 17:38:50 -0400

    Green test validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :email, presence: true, uniqueness: true 
+  validates :api_key, presence: true, uniqueness: true
 end
---
+++ db/migrate/20210809210306_add_email_column_to_user.rb
@@ -0,0 +1,5 @@
+class AddEmailColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :email, :string
+  end
+end
