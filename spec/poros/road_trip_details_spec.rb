8ed114f7ad6d90e81b06079e478440043d5a651d
Suzie K <suziekim.dev@gmail.com>
2021-08-10 17:05:28 -0400

    Red test sad path: returns less info

--- spec/poros/road_trip_details_spec.rb
+++ spec/poros/road_trip_details_spec.rb
@@ -20,8 +20,29 @@ RSpec.describe RoadTripDetails do
         expect(rtd.start_city).to eq('denver,co')
         expect(rtd.end_city).to eq('estes park,co')
         expect(rtd.travel_time).to eq('01 hours, 22 minutes')
-        expect(rtd.weather_at_eta[:temperature]).to eq('81.6 F')
+        expect(rtd.weather_at_eta[:temperature]).to eq('81.8 F')
         expect(rtd.weather_at_eta[:conditions]).to eq('few clouds')
+      end
+
+      it 'sad path: returns different message if the two cities are too far apart', :vcr do
+        forecast = WeatherFacade.forecast("london,uk")
+        time = Time.now + 5605
+
+        details = {
+          :start_city=>"denver,co",
+          :end_city=>"london,uk",
+          :travel_time=>nil,
+          :weather=> forecast,
+          :time=>nil }
+
+        rtd = RoadTripDetails.new(details)
+        
+        expect(rtd.id).to eq(nil)
+        expect(rtd.start_city).to eq('denver,co')
+        expect(rtd.end_city).to eq('london,uk')
+        expect(rtd.travel_time).to eq('impossible route')
+        expect(rtd.weather_at_eta[:temperature]).to eq(nil)
+        expect(rtd.weather_at_eta[:conditions]).to eq(nil)
       end
     end
   end
