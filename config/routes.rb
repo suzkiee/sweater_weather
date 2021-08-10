50873ff1fd4c9735a3add23d1f5615f8e752f8f8
Suzie K <suziekim.dev@gmail.com>
2021-08-10 16:15:16 -0400

    Green test can returns road trip info

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
       resources :users, only: [:create]
-      resources :sessions, only: [:create] 
+      resources :sessions, only: [:create]
+      resources :road_trip, only: [:create]
     end
   end
 end
