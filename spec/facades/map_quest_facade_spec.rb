f41035ea592e4d612aafe9f5bcc586d161c17b4a
Suzie K <suziekim.dev@gmail.com>
2021-08-10 16:18:16 -0400

    Green test returns latitude and longitude

--- spec/facades/map_quest_facade_spec.rb
+++ spec/facades/map_quest_facade_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MapQuestFacade do 
-describe 'class methods' do
-  describe '::coordinates' do
-    it 'returns latitude and longitude', :vcr do
-      coordinates = MapQuestFacade.coordinates('boston, ma') 
-        
-      expect(coordinates).to be_a(Hash)
-      expect(coordinates).to have_key(:lat)
-      expect(coordinates).to have_key(:lng)
-      expect(coordinates).to_not have_key(:locations)
+  describe 'class methods' do
+    describe '::coordinates' do
+      it 'returns latitude and longitude', :vcr do
+        coordinates = MapQuestFacade.coordinates('boston, ma') 
+          
+        expect(coordinates).to be_a(Hash)
+        expect(coordinates).to have_key(:lat)
+        expect(coordinates).to have_key(:lng)
+        expect(coordinates).to_not have_key(:locations)
+      end
     end
   end
 end
