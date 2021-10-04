0110c6e49dc80f96b4324f0416ed8853710edfef
Suzie K <suziekim.dev@gmail.com>
2021-10-04 15:25:46 -0400

    [Refactor] Add BaseService

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -1,9 +1,9 @@
-class BackgroundService
+class BackgroundService < BaseService
 
   def self.search_photos(location)
     new.request_api("/search/photos", location)
   end
- 
+
   def request_api(path, location)
     resp = conn("https://api.unsplash.com/").get(path) do |faraday|
       faraday.headers['Accept-Version'] = 'v1'
@@ -11,15 +11,6 @@ class BackgroundService
       faraday.params['query'] = location
       faraday.params['per_page'] = 1 
     end
-    
     parse_json(resp)
-  end 
-
-  def parse_json(response)
-    JSON.parse(response.body, symbolize_names: true) 
-  end
-
-  def conn(url)
-    Faraday.new(url)
   end
 end
